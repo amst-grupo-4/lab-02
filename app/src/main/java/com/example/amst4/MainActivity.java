a46f56d0ccc7f6a5adbbc7c31f152fe4ce6c1b70
drmendoz <drmendoz@espol.edu.ec>
2020-11-06 11:54:25 -0500

    Menu principal

--- app/src/main/java/com/example/amst4/MainActivity.java
+++ app/src/main/java/com/example/amst4/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.amst4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Intent intent;
+//        if(v.getId()==R.id.btnYoutube){
+//             intent = new Intent(this, formulario_registro.class);
+//            startActivity(intent);
+//        }else if(v.getId()==R.id.btnCalendario){
+//             intent = new Intent(this, formulario_registro.class);
+//
+//        }else if (v.getId()==R.id.btnMapa){
+//            intent = new Intent(this, formulario_registro.class);
+//        }else{
+            intent = new Intent(this, GraficoLineal.class);
+
+
+        startActivity(intent);
     }
 }
